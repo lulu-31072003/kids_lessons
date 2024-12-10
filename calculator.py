ef18aa46422e15bbc23fc83bbd1d8d07151901a6
Ľudmila Andradeová <milkauramova@gmail.com>
2024-12-10 16:28:39 +0100

    adding another lesson

---
+++ calculator.py
@@ -0,0 +1,33 @@
+'''
+V tomto programe si napíšeš funkciu calculator, ktorá prijíma 3 vstupné argumenty:
+    - num_1: nejaké číslo
+    - num_2: nejaké číslo
+    - operation: matematická operácia, napríklad +, -, *, /
+
+Funkcia bude teda fungovať ako kalkulačka: používateľ zadá 2 čísla a operáciu,
+ktorá sa má s nimi vykonať. Napríklad pre vstupy 1, 5 a +, funkcia vráti 6, lebo 1 + 5 = 6
+
+Nezabudni však ošetriť prípad, kedy by používateľ delil nulou.
+Ak sa používateľ pokúsi deliť nulou, upozorni ho na to!
+
+Ku funkcii calculator si napíš aj vlastné testy,
+aby sme si boli istí, že funckia funguje správne.
+
+Na zamyslenie:
+    Aký hraničný prípad by trebalo testovať (okrem iných testov)?
+'''
+
+def calculator(num_1, num_2, operation):
+    pass
+
+
+def calculator_test():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ debbuging.py
@@ -0,0 +1,61 @@
+# this function decides, if the given number is prime number
+def is_prime(num):
+    if num <= 1:
+        return False
+    
+    i = 2
+    while i < num // 2 + 1:
+        if num % i == 0:
+            return False
+    return True
+
+
+# test function for is_prime function
+def is_prime_test():
+    assert not is_prime(1)
+    print("Everything works :D !")
+
+
+# finish the following function so that it returns result of num % 7
+# don't you "%" operator though
+def modulo_7(num):
+    pass
+
+
+# finish test function for modulo_7 function
+def modulo_7_test():
+    assert modulo_7(9) == 2
+
+
+# finish the following function so that it returns sum of digits of the given number
+# for 123 -> 1 + 2 + 3 = 6
+def digit_sum(num):
+    pass
+
+
+# write test function for digit_sum function
+def digit_sum_test():
+    pass
+
+
+# write test function for function that returns amount of occurences of "a" in the given word
+def amount_of_a_test():
+    pass
+
+
+# now, write the function which returns amount of occurrences of "a" in the given word
+# "abrakadabra" -> 5
+# hint: strings are also lists in python
+def amount_of_a(word):
+    pass
+
+
+def main():
+    # you can call your functions here
+
+    print(is_prime(1))
+    is_prime_test()
+
+
+if __name__ == "__main__":
+    main()
